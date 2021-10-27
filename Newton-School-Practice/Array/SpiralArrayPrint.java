b1ff89517de576841d7be5f693d8230b61bc90b3
siddharthgopakumar <61907100+siddharthgopakumar@users.noreply.github.com>
2021-10-27 21:22:37 +0530

    Debugged printing spiral

--- Newton-School-Practice/Array/SpiralArrayPrint.java
+++ Newton-School-Practice/Array/SpiralArrayPrint.java
@@ -12,33 +12,34 @@ public class SpiralArrayPrint {
    {
        twoDArray tDArr = new twoDArray();
        int[][] arr= tDArr.scanTwoDArray();
-       int k = 0, p = arr[0].length - 1, l = arr.length - 1;
-       while( k <= p)
+       int k = 0, p = arr[0].length - 1, l = arr.length - 1, count = 0;
+       while( count < arr.length * arr[0].length )
        {
            int j = k, i;
            for(i = k; i <= l; i++)
            {
                System.out.print(arr[i][j]  + " ");
+               count++;
            }
            i--; 
            for( j = k+1; j <= p; j++)
            {
                
-                   System.out.print(arr[i][j]  + " ");
-               
+                  System.out.print(arr[i][j]  + " ");
+                  count++;
            }
            j--;
-           for(i = p-1; i >= k; i--)
+           for(i = l-1; i >= k; i--)
            {
                System.out.print( arr[i][j]  + " ");
-               
+               count++;
            }
            i++;
-           for( j = l-1; j >= k+1; j--)
+           for( j = p-1; j >= k+1; j--)
            {
                
                    System.out.print(arr[i][j] + " ");
-               
+                   count++;
            }
            k++;
            p--; 
