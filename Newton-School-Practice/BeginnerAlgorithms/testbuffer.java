7cc4a6ac91cf69bf7503ee358820dfed84befe77
siddharthgopakumar <61907100+siddharthgopakumar@users.noreply.github.com>
2021-10-31 14:30:32 +0530

    KOperationsOnN

---
+++ Newton-School-Practice/BeginnerAlgorithms/testbuffer.java
@@ -0,0 +1,16 @@
+package BeginnerAlgorithms;
+
+
+/**
+ * Write a description of testbuffer here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class testbuffer {
+
+    public void testbuffer1()
+    {
+        System.out.println(Math.log10(5));
+    }
+}
