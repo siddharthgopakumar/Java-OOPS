d8722d1edbffdc7544b428d658cb86be0f350fd2
siddharthgopakumar <61907100+siddharthgopakumar@users.noreply.github.com>
2021-11-07 11:44:33 +0530

    Update longestRepeatingString.java

--- Newton-School-Practice/Strings/longestRepeatingString.java
+++ Newton-School-Practice/Strings/longestRepeatingString.java
@@ -23,13 +23,6 @@ public class longestRepeatingString {
             for( int j =1; j <= N; j++)
             {
                 currString = testString.substring(idxI,idxI+i);
-                //else
-                //  {
-                //     currString = testString.substring(idxI, testString.indexOf(" ", idxI));
-                // }
-                //System.out.println("Current String " + currString);
-                //System.out.println("Search String" + " " + testString.substring(0, 0 + i));
-                //System.out.println(testString.substring(0, 0 + i).equals(currString));
                 if(!testString.substring(0, 0 + i).equals(currString))
                 {
                     break;
@@ -44,20 +37,14 @@ public class longestRepeatingString {
                 idxI =  testString.indexOf(" ",idxI) + 1;
 
             }
-            //System.out.println("Minimum length: " + minLen); 
-            //System.out.println("repeat ");
             if(repeat == N)
             {
                 longstringidx = i;
                 lastRepeat = repeat;
             }
             idxI = 0;
-
             repeat = 0;
         }
-
-        //System.out.println("minlength " + minLen);
-        //System.out.println(repeat);
         System.out.println((lastRepeat == N)? testString.substring(0,longstringidx):-1);
     }
 }
